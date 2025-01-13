23400d352d2241a176b946e3ea8a332f4942d212
prernagoswami <prernag.sky@gmail.com>
2025-01-13 11:13:05 +0530

    Added Orbit Controls

--- vite_react/src/components/CanvasLoader.jsx
+++ vite_react/src/components/CanvasLoader.jsx
@@ -1,24 +1,32 @@
-import { Html,useProgress} from "@react-three/drei"
+import { Html, useProgress } from "@react-three/drei";
 
 const CanvasLoader = () => {
-    const {progress} = useProgress()
+    const { progress } = useProgress();
 
     return (
-         <Html
-              as="div"
-              center 
-              style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  flexDirection: 'column'
-              }}          
+        <Html
+            as="div"
+            center
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+            }}
         >
-            <spam className="canvas-loader"/>
-            <p style={{fontSize: 14,color: '#f1f1f1', fontWeight: 800, marginTop: 40}}>
-            {progress !== 0 ? 'Loading...':`${progress.toFixed(2)}%` }
+            <span className="canvas-loader" />
+            <p
+                style={{
+                    fontSize: 14,
+                    color: "#f1f1f1",
+                    fontWeight: 800,
+                    marginTop: 40,
+                }}
+            >
+                {progress !== 0 ? `${progress.toFixed(2)}%` : "Loading..."}
             </p>
-            </Html>
-    )
-}
-export default CanvasLoader
+        </Html>
+    );
+};
+
+export default CanvasLoader;
